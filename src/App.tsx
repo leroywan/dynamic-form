ccb4f5fa6bc095cdee2b7839eba20b085ae0c6a6
Leroy Wan <leroywan@gmail.com>
2021-07-06 13:56:04 -0400

    Add test data

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import { DynamicForm } from './components'
+import {testData} from './testData'
 
 function App() {
   return (
     <div className="App">
       <DynamicForm name="Name" />
+      <pre>{JSON.stringify(testData, null, 2)}</pre>
     </div>
   );
 }
