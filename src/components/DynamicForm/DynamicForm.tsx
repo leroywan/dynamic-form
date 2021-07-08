654dae818af49a49fd92a294e10bf2a32207fe7f
Leroy Wan <leroywan@gmail.com>
2021-07-07 23:57:43 -0400

    Add form logic

--- src/components/DynamicForm/DynamicForm.tsx
+++ src/components/DynamicForm/DynamicForm.tsx
@@ -1,60 +1,101 @@
-import React from 'react'
-import { FieldErrors, useForm, UseFormRegister } from "react-hook-form";
+import React from "react";
+import {
+  FieldError,
+  FieldErrors,
+  useForm,
+  UseFormRegister,
+  UseFormWatch,
+} from "react-hook-form";
 
-import './DynamicForm.css'
+import "./DynamicForm.css";
 
-export type FieldTags = "input" | "select" | "textarea"
+export type FieldTags = "input" | "select" | "textarea";
 
 export type InputType =
-"button" |
-"checkbox" |
-"color" |
-"date" |
-"datetime-local" |
-"email" |
-"file" |
-"hidden" |
-"image" |
-"month" |
-"number" |
-"password" |
-"radio" |
-"range" |
-"reset" |
-"search" |
-"submit" |
-"tel" |
-"text" |
-"time" |
-"url" |
-"week"
+  | "button"
+  | "checkbox"
+  | "color"
+  | "date"
+  | "datetime-local"
+  | "email"
+  | "file"
+  | "hidden"
+  | "image"
+  | "month"
+  | "number"
+  | "password"
+  | "radio"
+  | "range"
+  | "reset"
+  | "search"
+  | "submit"
+  | "tel"
+  | "text"
+  | "time"
+  | "url"
+  | "week";
 
 export interface Conditional {
-  name: string
-  show_if: (val?: any) => boolean
+  name: string;
+  show_if: (val?: any) => boolean;
 }
 
 export interface FormField {
-  tag: FieldTags
-  name: string
-  type: InputType
-  human_label: string
-  conditional?: Conditional
-  required?: boolean
+  tag: FieldTags;
+  name: string;
+  type: InputType;
+  human_label: string;
+  conditional?: Conditional;
+  required?: boolean;
 }
 
 interface DynamicFormProps {
   formFields: FormField[];
 }
 
-function renderFormTag(field: FormField, register: UseFormRegister<any>, errors: FieldErrors) {
+function renderFormError(error: FieldError) {
+  if (!error) return null;
+
+  switch (error.type) {
+    case "required":
+      return <span className="form-field__error">This field is required</span>;
+    default:
+      return <span className="form-field__error">This field is invalid</span>;
+  }
+}
+
+function renderFormTag(
+  field: FormField,
+  register: UseFormRegister<any>,
+  watch: UseFormWatch<any>,
+  errors: FieldErrors
+) {
+  if (field.conditional) {
+    const targetValue = watch(field.conditional.name);
+    if (!targetValue || !field.conditional.show_if(targetValue)) {
+      return null;
+    }
+  }
+
   switch (field.tag) {
-    case 'input':
-      return <div className="form-field" key={field.name}>
-        <label className="form-field__label" htmlFor={field.name}>{field.human_label}</label>
-        <input className="form-field__input" id={field.name} type={field.type} {...register(field.name, { required: field.required })} />
-        {errors[field.name] && <span className="form-field__error">This field is required</span>}
-      </div>
+    case "input":
+      return (
+        <div className="form-field" key={field.name}>
+          <label className="form-field__label" htmlFor={field.name}>
+            {field.human_label}
+          </label>
+          <input
+            className="form-field__input"
+            id={field.name}
+            type={field.type}
+            {...register(field.name, {
+              required: field.required,
+              shouldUnregister: true,
+            })}
+          />
+          {renderFormError(errors[field.name])}
+        </div>
+      );
     // TODO: Add <select>
     // case 'select':
     //   return
@@ -62,16 +103,23 @@ function renderFormTag(field: FormField, register: UseFormRegister<any>, errors:
     // case 'textarea':
     //   return
     default:
-      return null
+      return null;
   }
 }
 
-export function DynamicForm({formFields}: DynamicFormProps) {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = (data: FormField) => console.log(data);
+export function DynamicForm({ formFields }: DynamicFormProps) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-  return <form className="form" onSubmit={handleSubmit(onSubmit)}>
-    {formFields.map((field) => renderFormTag(field, register, errors))}
-    <button className="form__submit">Submit</button>
-  </form>
+  const onSubmit = (data: FormField) => console.log(data);
+  return (
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      {formFields.map((field) => renderFormTag(field, register, watch, errors))}
+      <button className="form__submit">Submit</button>
+    </form>
+  );
 }
--- src/testData.ts
+++ src/testData.ts
@@ -1,47 +1,60 @@
-import {FormField} from './components/DynamicForm'
+import { FormField } from "./components/DynamicForm";
 
-const testData: FormField[] = [{
-  "tag": "input",
-	"name": "first_name",
-  "type": "text",
-	"human_label": "First Name",
-  "required": true
-}, {
-  "tag": "input",
-  "name": "last_name",
-  "type": "text",
-	"human_label": "Last Name"
-}, {
-  "tag": "input",
-  "name": "email",
-  "type": "email",
-  "human_label": "Email Address"
-}, {
-  "tag": "input",
-  "name": "phone_number",
-  "type": "text",
-  "human_label": "Phone Number"
-}, {
-  "tag": "input",
-  "name": "job_title",
-  "type": "text",
-  "human_label": "Job Title"
-}, {
-  "tag": "input",
-  "name": "date_of_birth",
-  "type": "date",
-  "human_label": "Date of Birth"
-}, {
-  "tag": "input",
-  "name": "parental_consent",
-  "type": "checkbox",
-  "human_label": "Parental Consent",
-	"conditional": {
-		"name": "date_of_birth",
-		"show_if": (value) => {
-      const now = new Date();
-			return value >= new Date(now.getFullYear() - 13, now.getMonth(), now.getDate());
-    }
-}}]
+const testData: FormField[] = [
+  {
+    tag: "input",
+    name: "first_name",
+    type: "text",
+    human_label: "First Name",
+    required: true,
+  },
+  {
+    tag: "input",
+    name: "last_name",
+    type: "text",
+    human_label: "Last Name",
+  },
+  {
+    tag: "input",
+    name: "email",
+    type: "email",
+    human_label: "Email Address",
+  },
+  {
+    tag: "input",
+    name: "phone_number",
+    type: "text",
+    human_label: "Phone Number",
+  },
+  {
+    tag: "input",
+    name: "job_title",
+    type: "text",
+    human_label: "Job Title",
+  },
+  {
+    tag: "input",
+    name: "date_of_birth",
+    type: "date",
+    human_label: "Date of Birth",
+  },
+  {
+    tag: "input",
+    name: "parental_consent",
+    type: "checkbox",
+    human_label: "Parental Consent",
+    conditional: {
+      name: "date_of_birth",
+      show_if: (value) => {
+        const dateInfo = value.split(/\D/);
+        const now = new Date();
+        return (
+          new Date(dateInfo[0], dateInfo[1], dateInfo[2]) >=
+          new Date(now.getFullYear() - 13, now.getMonth(), now.getDate())
+        );
+      },
+    },
+  },
+];
 
-export {testData};
+export { testData };
